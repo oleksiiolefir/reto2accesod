572703d387cb0396581110080d61ae08c68e83aa
jonbenja2 <jonbenja2@gmail.com>
2019-10-29 12:10:18 +0100

    txt

--- reto2accesod/src/modelo/Consultas.java
+++ reto2accesod/src/modelo/Consultas.java
@@ -63,48 +63,55 @@ public class Consultas {
 	 }
 	 
 	 //METER POR PARAMETRO EL OBJETO DE Empleado
-	 public void insertarEmpleados(ArrayList<Empleado> emple) {
-
+	
+	
+	public void insertarEmpleados(ArrayList<Empleado> emple) throws SQLException {
+		  for (int i = 0; i < emple.size(); ++i) {					    
+			    
+		   
 			PreparedStatement stmt = null;
-			ResultSet result = null;
+			
 			
 			String query;
-		
-			query = "INSERT INTO empleado (ID, NOMBRE, APELLIDOS, SUELDO, BOSS,COD_DEPT,JEFE, PUESTO) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-
+			
+			query = "INSERT INTO empleado (ID, NOMBRE, APELLIDOS, SUELDO, BOSS,COD_DEPT,JEFE, PUESTO) VALUES ('"+emple.get(i).getId()+"','"+emple.get(i).getNombre()+"',"
+					+ "'"+emple.get(i).getApellido()+"', '"+emple.get(i).getSueldo()+"','"+ emple.get(i).getBoss()+"', '"+ emple.get(i).getCod_dept()+"',"
+							+ "'"+ 0+"', '"+ emple.get(i).getPuesto()+"')";
+	
+		/*	stmt.setInt(1, emple.get(i).getId());
+		    stmt.setString(2,emple.get(i).getNombre());
+		    stmt.setString(3, emple.get(i).getApellido());
+		    stmt.setInt(4, emple.get(i).getSueldo());
+		    stmt.setInt(5, emple.get(i).getBoss());
+		    stmt.setInt(6, emple.get(i).getCod_dept());
+		    stmt.setBoolean(7, emple.get(i).getJefe());
+		    stmt.setString(8, emple.get(i).getPuesto());*/
 			try {
 
 			    // abrimos una conexion
 			    connection = conexion.conectar();
-
-			    // preparamos la consulta INSERT
 			    stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-			    // añadimos los valores a insertar
-
-			    for (int i = 0; i < emple.size(); ++i) {					    
-				    stmt.setInt(1, emple.get(i).getId());
-				    stmt.setString(2,emple.get(i).getNombre());
-				    stmt.setString(3, emple.get(i).getApellido());
-				    stmt.setInt(4, emple.get(i).getSueldo());
-				    stmt.setInt(5, emple.get(i).getBoss());
-				    stmt.setInt(6, emple.get(i).getCod_dept());
-				    stmt.setBoolean(7, emple.get(i).getJefe());
-				    stmt.setString(8, emple.get(i).getPuesto());
-			    }
+			    // preparamos la consulta INSERT
 			    
+			    // añadimos los valores a insertar
 			    stmt.executeUpdate();
 			    // Ejecuta la consulta y guarda los resultados en un objeto ResultSet
 			   
 
 			} catch (SQLException e) {
 			    e.printStackTrace();
-			} finally {
+			} 
+			finally {
 			    try {
-				connection.close();
-			    } catch (Exception e) {
+			    	
+			    } 
+			    catch (Exception e) {
 				e.printStackTrace();
 			    }
 			}
+			
+		  }
+		  connection.close();
 	 }
 }
 	 

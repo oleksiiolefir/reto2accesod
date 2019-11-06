1618f917154d262b2770d94ab29e141e1b85e541
IN1DM3B_13 <IN1DM3B_13@H015112.NET>
2019-11-06 14:16:16 +0100

    Cambios controlador crear usuario

--- reto2accesod/src/controlador/controladorCrearUsuario.java
+++ reto2accesod/src/controlador/controladorCrearUsuario.java
@@ -3,6 +3,9 @@ package controlador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
 import vista.JframePrincipal;
 
 public class controladorCrearUsuario implements ActionListener{
@@ -19,21 +22,28 @@ public class controladorCrearUsuario implements ActionListener{
 	 */
 	public void addListeners() {
 		vista.crearUsuario.btnAgregar.addActionListener(this);
-	}
-	
-	public void datosCorrectos() {
-		try {
-			
-		} catch(Exception e) {
-			
-		}
-	}
+	}	
 	
 	 /**
 	 * Acción de los distintos listeners
 	 */
 	public void actionPerformed(ActionEvent e) {
-		vista.menu.setVisible(true);
-		vista.bienvenida.setVisible(false);
+		
+		Object sourceObject = e.getSource();
+		
+		if (sourceObject instanceof JButton) {
+			
+			String botonPulsado = ((JButton) sourceObject).getActionCommand();
+			
+			switch(botonPulsado) {
+			case "GUARDAR":
+				vista.menu.setVisible(true);
+				vista.bienvenida.setVisible(false);
+				break;
+			case "prueba":
+				
+				break;
+			}
+		}
 	}
 }

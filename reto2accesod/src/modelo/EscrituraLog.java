dc144911def9ec4adbadf1bcb9ed47abb6318304
IN1DM3B_06 <IN1DM3B_06@H015112.NET>
2019-11-18 12:52:22 +0100

    La clase log

---
+++ reto2accesod/src/modelo/EscrituraLog.java
@@ -0,0 +1,39 @@
+package modelo;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class EscrituraLog {
+
+	public void crearLog(Date fecha, String motivo, String metodo, String clase) {
+		String ruta = "ficheros" + File.separator + "log.txt"; //Guarda la ruta del fichero
+		FileWriter fichero = null;
+		PrintWriter escritor = null;
+		try {
+			File log = new File(ruta); //Busca si existe el fichero en la ruta especificada
+			if(!log.exists())
+				JOptionPane.showMessageDialog(new JFrame(), "Fichero log.txt no encontrado.", null, 0);
+			fichero = new FileWriter(ruta, true); 
+			escritor = new PrintWriter(fichero);
+			
+			//Se escribe en el archivo
+			escritor.println("Fecha: " + fecha + " *** La clase es : " + clase + " *** El metodo es : " + metodo + " *** el motivo por el cual ocurre el  error: " + motivo);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally { //Una vez terminado, se cierra el fichero
+			try {
+				fichero.close();
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}

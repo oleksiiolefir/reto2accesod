f60e10597800623789ee581007d90e7db7393b75
IN1DM3B_13 <IN1DM3B_13@H015112.NET>
2019-11-19 09:34:15 +0100

    reestructuracion de generador txt

--- reto2accesod/src/modelo/GenerarTXT.java
+++ reto2accesod/src/modelo/GenerarTXT.java
@@ -17,21 +17,45 @@ public class GenerarTXT {
 	            }
 	            FileWriter fw = new FileWriter(file);
 	            BufferedWriter bw = new BufferedWriter(fw);
-	            bw.write("EMPLEADOS");
+	            bw.write("EMPLEADOS:");
+	            bw.newLine();
 	            bw.newLine();
 	            for(int i=0; i<lista.size(); i++) {
-	            	bw.write(lista.get(i).getId() + ";" + lista.get(i).getNombre() + ";" + lista.get(i).getApellido() + ";"
-	            		+ lista.get(i).getSueldo() + ";" + lista.get(i).getBoss() + ";" + lista.get(i).getCod_dept() + ";"
-	            		+ lista.get(i).getJefe() + ";" + lista.get(i).getPuesto());
+	            	bw.write("***************** Empleado " + (i+1)  + " *****************");
 	            	bw.newLine();
+	            	bw.write("ID = " + lista.get(i).getId());
+		            bw.newLine();
+		            bw.write("NOMBRE = " + lista.get(i).getNombre());
+		            bw.newLine();
+		            bw.write("APELLIDO = " + lista.get(i).getApellido());
+		            bw.newLine();
+		            bw.write("SUELDO = " + lista.get(i).getSueldo());
+		            bw.newLine();
+		            bw.write("BOSS = " + lista.get(i).getBoss());
+		            bw.newLine();
+		            bw.write("CODIGO DEPARTAMENTO = " + lista.get(i).getCod_dept());
+		            bw.newLine();
+		            bw.write("JEFE = " + lista.get(i).getJefe());
+		            bw.newLine();
+		            bw.write("PUESTO = " + lista.get(i).getPuesto());
+		            bw.newLine();
+		            bw.newLine();
 	            }
 	            
-	            bw.write("-----------------------------------------------------------------------");
+	            bw.write("-----------------------------------------------------------------");
 	            bw.newLine();
-	            bw.write("DEPARTAMENTOS");
+	            bw.newLine();
+	            bw.write("DEPARTAMENTOS:");
+	            bw.newLine();
 	            bw.newLine();
 	            for(int i=0; i<Departamento.size(); i++) {
-	            	bw.write(Departamento.get(i).getCod_dept() + ";" + Departamento.get(i).getLugar()+";"+ Departamento.get(i).getDnombre() );
+	            	bw.write("***************** Departamento " + (i+1) + " *****************");
+	            	bw.newLine();
+	            	bw.write("CODIGO DEPARTAMENTO = " + Departamento.get(i).getCod_dept());
+	            	bw.newLine();
+	            	bw.write("LUGAR = " + Departamento.get(i).getLugar());
+	            	bw.newLine();
+	            	bw.write("NOMBRE DEPARTAMENTO " + Departamento.get(i).getDnombre());
 	            	bw.newLine();
 	            }
 	            bw.close();
